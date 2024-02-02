7c3d741dca7f720208e372259f39b70be18823f8
Florian Wientzek <florian.wientzek@gmx.net>
2024-02-02 19:46:00 +0100

    Minimal PDF file check.

--- src/main/java/com/hauser/dochausersrv/repository/FileRepository.java
+++ src/main/java/com/hauser/dochausersrv/repository/FileRepository.java
@@ -19,9 +19,23 @@ public class FileRepository {
     private String importPath;
 
     public void importFile(MultipartFile file) throws IOException {
-        final UUID fileId = UUID.randomUUID();
-        final String fileName = importPath + fileId + ".pdf";
-        LOG.info("Uploading file " + file.getOriginalFilename() + " with name " + fileName);
-        file.transferTo(new File(fileName));
+        if (isPdfFileByContent(file)) {
+            final UUID fileId = UUID.randomUUID();
+            final String fileName = importPath + fileId + ".pdf";
+            LOG.info("Uploading file " + file.getOriginalFilename() + " with name " + fileName);
+            file.transferTo(new File(fileName));
+        } else {
+            LOG.error("The uploaded file " + file.getOriginalFilename() + " is not a pdf file and is therefore discarded.");
+        }
+    }
+
+    private boolean isPdfFileByContent(MultipartFile file) {
+        try {
+            byte[] fileBytes = file.getBytes();
+            String header = new String(fileBytes, 0, Math.min(fileBytes.length, 5));
+            return header.startsWith("%PDF-");
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
